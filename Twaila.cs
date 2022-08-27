a10a830b1b796130e08e4904a3e091000f5a95ca
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2022-08-27 10:40:52 -0400

    Removed Obsolete Method
    Twaila.PostAddRecipes was moved to ItemTilePairSystem.PostAddRecipes

--- Twaila.cs
+++ Twaila.cs
@@ -16,15 +16,9 @@ namespace Twaila
             ExtraObjectData.Initialize();
         }
 
-        public override void PostAddRecipes()
-        {
-            ItemUtil.Load(); // loaded at this point so that all items from all mods have been loaded
-        }
-
         public override void Unload()
         {
             ExtraObjectData.Unload();
-            ItemUtil.Unload();
         }
     }
 }
